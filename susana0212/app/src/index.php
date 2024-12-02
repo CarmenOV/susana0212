b3a0e4a13796bcb7763b1ce4a24ae619d0711242
Carmen Ortega <131663078+CarmenOV@users.noreply.github.com>
2024-12-02 22:53:58 +0100

    Delete susana0212 directory
    error de subida de archivos

--- susana0212/app/src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Muestra la fecha y hora actual
-echo "<h1>Bienvenido a mi aplicación PHP</h1>";
-echo "<p>Esta página fue generada dinámicamente usando PHP.</p>";
-echo "<p>Fecha y hora actual: " . date('Y-m-d H:i:s') . "</p>";
-
-// Información de PHP (puedes descomentarlo para ver la configuración de PHP en tu servidor)
-// phpinfo();
-?>
